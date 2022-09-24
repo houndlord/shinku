b1dbbe7e183e2e899da2e6c805f955216532f2f5
Lev E. Chechulin <l.e.chechulin@gmail.com>
2022-09-24 14:02:02 +0200

    more tests for walk.py

--- tests/walk_tree_test.py
+++ tests/walk_tree_test.py
@@ -74,7 +74,6 @@ def setup_dirs():
     create_log_dir()
 
 
-"""
 def test_walk_basic():
     setup_dirs()
     walk.walk('./testdir', './dst', './log/log')
@@ -87,158 +86,31 @@ def test_walk_basic():
     rm_log_dir()
 
 
-def test_walk_nested():
+def test_dir_creation():
+    setup_dirs()
+    walk.walk('./testdir', './dst', './log/log')
+    assert os.stat('./testdir/nesteddir1').st_size == os.stat(
+        './dst/nesteddir1').st_size
+    delete_test_dirs()
+    rm_log_dir()
+
+
+def test_backwalk_basic():
+    setup_dirs()
+    walk.walk('./testdir', './dst', './log/log')
+    os.rmdir('./testdir/nesteddir1')
+    walk.backwalk('./testdir', './dst', './log/log')
+    assert len(os.listdir('./dst')) == 2
+    delete_test_dirs()
+    rm_log_dir()
+
+
+def test_backwalk_nested():
     setup_dirs()
     create_test_file('./testdir/nesteddir1/f')
     walk.walk('./testdir', './dst', './log/log')
-    f1 = open('./testdir/nesteddir1/f')
-    f2 = open('./dst/nesteddir1/f')
-    assert f1.read() == f2.read()
-    f1.close()
-    f2.close()
+    os.remove('./testdir/nesteddir1/f')
+    walk.backwalk('./testdir', './dst', './log/log')
+    assert len(os.listdir('./dst/nesteddir1')) == 0
     delete_test_dirs()
     rm_log_dir()
-
-
-def test_walk_file_change_basic():
-    setup_dirs()
-    walk.walk('./testdir', './dst', './log/log')
-    f1 = open('./testdir/f', 'a')
-    f1.write(gen_random_string_len(512))
-    f1.close()
-    walk.walk('./testdir', './dst', './log/log')
-    f1 = open('./testdir/f')
-    f2 = open('./dst/f')
-    assert f1.read() == f2.read()
-    f1.close()
-    f2.close()
-    delete_test_dirs()
-    rm_log_dir()
-
-
-def test_walk_file_change_nested():
-    setup_dirs()
-    create_test_file('./testdir/nesteddir1/f')
-    walk.walk('./testdir', './dst', './log/log')
-    f1 = open('./testdir/nesteddir1/f', 'a')
-    f1.write(gen_random_string_len(512))
-    f1.close()
-    walk.walk('./testdir', './dst', './log/log')
-    f1 = open('./testdir/nesteddir1/f')
-    f2 = open('./dst/nesteddir1/f')
-    assert f1.read() == f2.read()
-    f1.close()
-    f2.close()
-    delete_test_dirs()
-    rm_log_dir()
-
-
-def test_big_files():
-    setup_dirs()
-    create_test_file_len('./testdir/nesteddir1/f', 171824)
-    create_test_file_len('./testdir/nesteddir1/f1', 1421824)
-    create_test_file_len('./testdir/nesteddir2/f', 178224)
-    create_test_file_len('./testdir/nesteddir2/f1', 118224)
-    walk.walk('./testdir', './dst', './log/log')
-
-    assert os.stat('./testdir/nesteddir1/f').st_size == os.stat(
-        './dst/nesteddir1/f').st_size
-
-    assert os.stat('./testdir/nesteddir1/f1').st_size == os.stat(
-        './dst/nesteddir1/f1').st_size
-
-    assert os.stat('./testdir/nesteddir2/f').st_size == os.stat(
-        './dst/nesteddir2/f').st_size
-
-    assert os.stat('./testdir/nesteddir2/f1').st_size == os.stat(
-        './dst/nesteddir2/f1').st_size
-
-    delete_test_dirs()
-    rm_log_dir()
-
-
-def test_big_files_2():
-    setup_dirs()
-    create_test_file_len('./testdir/nesteddir1/f', 1721824)
-    create_test_file_len('./testdir/nesteddir1/f1', 1421824)
-    create_test_file_len('./testdir/nesteddir1/f2', 1421824)
-    create_test_file_len('./testdir/nesteddir1/f3', 1421824)
-    create_test_file_len('./testdir/nesteddir2/f', 1718124)
-    create_test_file_len('./testdir/nesteddir2/f1', 118224)
-    create_test_file_len('./testdir/nesteddir2/f2', 1428524)
-    walk.walk('./testdir', './dst', './log/log')
-    assert os.stat('./testdir/nesteddir1/f').st_size == os.stat(
-        './dst/nesteddir1/f').st_size
-
-    assert os.stat('./testdir/nesteddir1/f1').st_size == os.stat(
-        './dst/nesteddir1/f1').st_size
-
-    assert os.stat('./testdir/nesteddir1/f2').st_size == os.stat(
-        './dst/nesteddir1/f2').st_size
-
-    assert os.stat('./testdir/nesteddir1/f3').st_size == os.stat(
-        './dst/nesteddir1/f3').st_size
-
-    assert os.stat('./testdir/nesteddir2/f').st_size == os.stat(
-        './dst/nesteddir2/f').st_size
-
-    assert os.stat('./testdir/nesteddir2/f1').st_size == os.stat(
-        './dst/nesteddir2/f1').st_size
-
-    assert os.stat('./testdir/nesteddir2/f2').st_size == os.stat(
-        './dst/nesteddir2/f2').st_size
-
-    delete_test_dirs()
-    rm_log_dir()
-
-
-def test_big_files_3():
-    setup_dirs()
-    os.mkdir('./testdir/nesteddir1/n')
-    create_test_file_len('./testdir/nesteddir1/f', 1721824)
-    create_test_file_len('./testdir/nesteddir1/f1', 1421824)
-    create_test_file_len('./testdir/nesteddir1/f2', 1421824)
-    create_test_file_len('./testdir/nesteddir1/f3', 14218524)
-    create_test_file_len('./testdir/nesteddir1/f4', 14218524)
-    create_test_file_len('./testdir/nesteddir1/n/f', 14218524)
-    create_test_file_len('./testdir/nesteddir1/n/f1', 14218524)
-    create_test_file_len('./testdir/nesteddir1/n/f2', 14218524)
-    create_test_file_len('./testdir/nesteddir2/f', 1718224)
-    create_test_file_len('./testdir/nesteddir2/f1', 118224)
-    create_test_file_len('./testdir/nesteddir2/f2', 1428524)
-    walk.walk('./testdir', './dst', './log/log')
-    assert os.stat('./testdir/nesteddir1/f').st_size == os.stat(
-        './dst/nesteddir1/f').st_size
-
-    assert os.stat('./testdir/nesteddir1/f1').st_size == os.stat(
-        './dst/nesteddir1/f1').st_size
-
-    assert os.stat('./testdir/nesteddir1/f2').st_size == os.stat(
-        './dst/nesteddir1/f2').st_size
-
-    assert os.stat('./testdir/nesteddir1/f3').st_size == os.stat(
-        './dst/nesteddir1/f3').st_size
-
-    assert os.stat('./testdir/nesteddir1/f4').st_size == os.stat(
-        './dst/nesteddir1/f4').st_size
-
-    assert os.stat('./testdir/nesteddir1/n/f').st_size == os.stat(
-        './dst/nesteddir1/n/f').st_size
-
-    assert os.stat('./testdir/nesteddir1/n/f1').st_size == os.stat(
-        './dst/nesteddir1/n/f1').st_size
-    assert os.stat('./testdir/nesteddir1/n/f2').st_size == os.stat(
-        './dst/nesteddir1/n/f2').st_size
-
-    assert os.stat('./testdir/nesteddir2/f').st_size == os.stat(
-        './dst/nesteddir2/f').st_size
-
-    assert os.stat('./testdir/nesteddir2/f1').st_size == os.stat(
-        './dst/nesteddir2/f1').st_size
-
-    assert os.stat('./testdir/nesteddir2/f2').st_size == os.stat(
-        './dst/nesteddir2/f2').st_size
-
-    delete_test_dirs()
-    rm_log_dir()
-"""
